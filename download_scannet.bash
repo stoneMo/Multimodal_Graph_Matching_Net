27d1e3701a59f713b1d010832c046e37c9254fc5
ShentongMo <shentonm@andrew.cmu.edu>
2021-04-08 11:15:34 +0800

    init

---
+++ download_scannet.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# download the scannet_data
+# https://drive.google.com/file/d/1IXL5dkMuti9FPaMsMkCVSX16PB99sL2T/view?usp=sharing
+
+fileid="1IXL5dkMuti9FPaMsMkCVSX16PB99sL2T"
+filename="scannet_data.tar.gz"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
