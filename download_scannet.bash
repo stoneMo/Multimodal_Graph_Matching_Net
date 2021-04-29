6047cecae468de4a841a21eeed55f67c7a2089fd
ShentongMo <shentonm@andrew.cmu.edu>
2021-04-30 01:01:05 +0800

    init

--- download_scannet.bash
+++ download_scannet.bash
@@ -5,5 +5,5 @@
 
 fileid="1IXL5dkMuti9FPaMsMkCVSX16PB99sL2T"
 filename="scannet_data.tar.gz"
-curl -c ./cookie -k -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+curl -k -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -k -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
